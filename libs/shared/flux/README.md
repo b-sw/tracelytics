0c1cc8b5ef2bf4ef188ae2a42c3a0bdedb4ff511
bartoszswitalski <bartoszswitalski@icloud.com>
2023-07-01 10:50:46 +0200

    Add core flux

---
+++ libs/shared/flux/README.md
@@ -0,0 +1,11 @@
+# shared-flux
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build shared-flux` to build the library.
+
+## Running unit tests
+
+Run `nx test shared-flux` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/shared/flux/src/lib/dispatcher.ts
@@ -0,0 +1,24 @@
+import { Class } from '@tracelytics/shared/types';
+import { injectable } from 'inversify';
+import { filter, Observable, Subject } from 'rxjs';
+
+export class DispatcherAction {
+    constructor(public readonly payload?: Record<string, unknown>) {}
+
+    isEqual(eventType: any): boolean {
+        return this instanceof eventType;
+    }
+}
+
+@injectable()
+export class Dispatcher {
+    readonly #bus = new Subject<DispatcherAction>();
+
+    emit(action: DispatcherAction) {
+        this.#bus.next(action);
+    }
+
+    on<T extends DispatcherAction>(action: Class<T>): Observable<T> {
+        return this.#bus.pipe(filter((dispatcherAction) => dispatcherAction.isEqual(action))) as Observable<T>;
+    }
+}
