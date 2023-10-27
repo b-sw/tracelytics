bff95eeed1c77756bff331af9c0cbcff4f5f48c3
bartoszswitalski <bartoszswitalski@icloud.com>
2023-10-27 16:55:49 +0200

    WIP: quick commit

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from 'react-dom/client';
 
 import { App } from './app/app';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/';
+axios.defaults.baseURL = 'https://tracelytics.toadres.pl/';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
